b4a0fdbac151c083bfff5b4934368ac1eee42da8
laniavea <laniavea@yahoo.com>
2025-05-27 16:14:09 +0300

    Articles db init functions

---
+++ scripts/a_init_articles.py
@@ -0,0 +1,41 @@
+import os
+import psycopg2
+
+import scripts_shared_functions
+
+def mod_init_articles(pg_data, raw_articles_text_path, table_name):
+    all_raw_articles = sorted(os.listdir(raw_articles_text_path))
+
+    conn = scripts_shared_functions.get_db_connetion(pg_data)
+    cur = conn.cursor()
+
+    for now_raw_article in all_raw_articles:
+        print(f"Processing: {now_raw_article}")
+        full_file_path = os.path.join(raw_articles_text_path, now_raw_article)
+        article_name = str(".".join(now_raw_article.split(".")[:-1]))
+
+        article_text = merge_paragraphs(full_file_path)
+    
+        cur.execute(f"SELECT 1 FROM {table_name} WHERE title = %s", (article_name,))
+        if not cur.fetchone():
+            cur.execute(f"INSERT INTO {table_name} (title, language, content) VALUES (%s, %s, %s)", (article_name, 'rus', article_text))
+            print(f"Inserted: {article_name}")
+        else:
+            print(f"Skipped (already exists): {article_name}")
+            
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def merge_paragraphs(text_file_path):
+    paragraphs = scripts_shared_functions.load_dict_from_json(text_file_path)["paragraphs"]
+    merged_text = ""
+
+    for i in paragraphs:
+        _, now_par = scripts_shared_functions.split_page_and_metadata(i)
+        merged_text += now_par
+    return merged_text   
+
+if __name__ == '__main__':
+    pg_data = scripts_shared_functions.get_pg_data()
+    mod_init_articles("./../txt_articles/Параграфы/", 'articles')
---
+++ scripts/scripts_shared_functions.py
@@ -0,0 +1,60 @@
+import os
+import psycopg2
+import json
+import re
+
+def get_pg_data():
+    try:
+        with open('../.env') as f:
+            for line in f:
+                if not line.strip() or line.startswith('#'):
+                    continue
+                key, value = line.strip().split('=', 1)
+
+                if key in os.environ:
+                    print(f"Skipping {key} because already found")
+                    continue
+                else:
+                    os.environ[key] = value
+    except:
+        pass
+    
+    db_params = {
+        "host": '' or os.environ['NLP_PG_HOST'].replace("'", "").replace('"', ''),
+        "port": '' or os.environ['NLP_PG_PORT'].replace("'", "").replace('"', ''),
+        "db_name": '' or os.environ['NLP_PG_DB_NAME'].replace("'", "").replace('"', ''),
+        "user": '' or os.environ['NLP_PG_USER'].replace("'", "").replace('"', ''),
+        "password": '' or os.environ['NLP_PG_PASSWORD'].replace("'", "").replace('"', ''),
+    }
+    
+    return db_params
+
+
+def get_db_connetion(pg_data):
+    return psycopg2.connect(
+        host=pg_data["host"],
+        port=int(pg_data["port"]),
+        dbname=pg_data["db_name"],
+        user=pg_data["user"],
+        password=pg_data["password"]
+    )
+
+def load_dict_from_json(path):
+    with open(path, 'r', encoding='utf-8') as json_file:
+        dictionary = json.load(json_file)
+    return dictionary
+
+
+def split_page_and_metadata(full_text):
+    if not full_text.startswith("[(page"):
+        return None, full_text
+
+    pattern = r'\[\(page \d+\) \(latest_image_id \d+\)\]'
+    match = re.search(pattern, full_text)
+    if match:
+        metadata = match.group(0)
+        other_content = re.sub(pattern, '', full_text).strip()
+        
+        return metadata, other_content
+    else:
+        return None, full_text
